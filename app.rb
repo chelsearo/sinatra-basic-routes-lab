02959e6c51baa25fd5db5424b41857e7f5027c69
Chelsea <croland711@yandex.com>
2020-12-17 12:22:44 -0800

    commit tree b3b990d9e37ac0dc1b1ba5773c9bd73e6149468d

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+       get '/name' do 
+        "My name is Chelsea"
+      end
+    
+      get '/hometown' do 
+        "My hometown is Kirkland"
+      end
+    
+      get '/favorite-song' do
+        "My favorite song is I Love The Night"
+      end 
 end
